54576b9d7eb3ec82922b3effad769cccedd81506
Igor Derevnin <shroom2207@yandex.ru>
2024-10-02 21:24:30 +0300

    Update Password.py

--- Password.py
+++ Password.py
@@ -42,11 +42,12 @@ def password_score(text):
     return score
 
 
-def main():
+def check_password(event, entry, label1):
+    score_value = password_score(entry.get())
+    label1.config(text='Рейтинг этого пароля: ' + str(score_value))
 
-    def check_password(event):
-        score_value = password_score(entry.get())
-        label1.config(text='Рейтинг этого пароля: ' + str(score_value))
+
+def main():
 
     root = Tk()
     root.title("Проверка пароля")
@@ -54,27 +55,25 @@ def main():
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
     x_coord = (screen_width // 2) - (200 // 2)
-    y_coord = (screen_height // 2) - (100 // 2)
+    y_coord = (screen_height // 2) - (80 // 2)
     root.geometry(f'220x80+{x_coord}+{y_coord}')
     root.resizable(False, False)
 
     password = StringVar(root)
 
     label0 = Label(root, text='Введите пароль:')
-
-    entry = Entry(root, show='*', textvariable=password)
-    entry.bind('<KeyRelease>', check_password)
-
     label0.grid(row=0, column=0)
-    entry.grid(row=0, column=1)
 
     label1 = Label(root, text='')
     label1.grid(row=1, column=0, columnspan=2, sticky='w')
+
+    entry = Entry(root, show='*', textvariable=password)
+    entry.bind('<KeyRelease>', lambda event: check_password(
+        event, entry, label1))
+    entry.grid(row=0, column=1)
 
     root.mainloop()
 
 
 if __name__ == '__main__':
     main()
-
-
